7efe3db64bd087e45c5ffd42a86833e8664765c3
Amon Sawamura <amotarao@gmail.com>
2023-07-03 12:08:02 +0900

    home: 型整理

--- src/core/home/index.ts
+++ src/core/home/index.ts
@@ -1,6 +1,5 @@
 import { fetchHome } from './fetchers';
 
 export const getHome = async (homeId: number, cookie: string) => {
-  const home = await fetchHome(homeId, cookie);
-  return home;
+  return fetchHome(homeId, cookie);
 };
