34eb673a16531e456e72775ca272f6037d873cf6
Amon Sawamura <amotarao@gmail.com>
2023-07-03 12:38:47 +0900

    newest-homes: add page arg

--- src/core/newest-homes/fetchers.ts
+++ src/core/newest-homes/fetchers.ts
@@ -3,8 +3,11 @@ import { notFoundMessage, userAgent } from '../../constants';
 import { parseHomeSearchPage } from './parsers';
 import { HomeForSearch } from './types';
 
-export const fetchNewestHomes = async (cookie: string): Promise<HomeForSearch[]> => {
-  const res = await fetch(`https://address.love/homes/search`, {
+export const fetchNewestHomes = async (cookie: string, page?: number): Promise<HomeForSearch[]> => {
+  const url = new URL(`https://address.love/homes/search`);
+  page !== undefined && url.searchParams.set('page', page.toString());
+
+  const res = await fetch(url, {
     headers: {
       cookie,
       'User-Agent': userAgent,
--- src/core/newest-homes/index.ts
+++ src/core/newest-homes/index.ts
@@ -1,5 +1,5 @@
 import { fetchNewestHomes } from './fetchers';
 
-export const getNewestHomes = async (cookie: string) => {
-  return fetchNewestHomes(cookie);
+export const getNewestHomes = async (cookie: string, page?: number) => {
+  return fetchNewestHomes(cookie, page);
 };
